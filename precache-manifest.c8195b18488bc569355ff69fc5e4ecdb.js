d5f365eb666bfb3f54373591e0b0f6d21154dbb1
Your Name <you@example.com>
2020-08-17 11:37:55 -0700

    Updates

---
+++ precache-manifest.c8195b18488bc569355ff69fc5e4ecdb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5aa2e7c3267e06fca1745f13c24c9e54",
+    "url": "/covid-data-visualizer/index.html"
+  },
+  {
+    "revision": "651dcd461850a767eeb1",
+    "url": "/covid-data-visualizer/static/css/main.115ef922.chunk.css"
+  },
+  {
+    "revision": "90e9df058a4bc8598aa7",
+    "url": "/covid-data-visualizer/static/js/2.4041a2ee.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/covid-data-visualizer/static/js/2.4041a2ee.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "651dcd461850a767eeb1",
+    "url": "/covid-data-visualizer/static/js/main.7d1fb8d7.chunk.js"
+  },
+  {
+    "revision": "5332ed6b24359bc73d37",
+    "url": "/covid-data-visualizer/static/js/runtime-main.1432ab01.js"
+  }
+]);
